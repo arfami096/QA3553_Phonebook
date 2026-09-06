aa00b22ccce2d2d382c54ac6912bbefc7372aa6c
Netto test <arfami@icloud.com>
2026-09-06 15:42:19 +0700

    loggerUpdated

--- utils/logger.py
+++ utils/logger.py
@@ -3,10 +3,23 @@ import logging
 
 def get_logger(name=__name__):
   logger = logging.getLogger(name)
-  # Если хэндлеров еще нет, просто задаем уровень.
-  # Никаких StreamHandler создавать НЕ нужно!
   if not logger.handlers:
     logger.setLevel(logging.INFO)
+
+    # Настраиваем понятный формат: дата/время, уровень, название модуля и сообщение
+    formatter = logging.Formatter(
+        "[%(asctime)s] [%(levelname)s] [%(name)s]: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    # Добавляем StreamHandler для вывода в поток (stderr/stdout)
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    # Отключаем всплытие к корневому логгеру, чтобы избежать лишних дублей
+    logger.propagate = False
+
   return logger
 
 #Логирование
