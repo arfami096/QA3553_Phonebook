d6a3daa0b372d47312a1b7854ef4004507a5d431
Netto test <arfami@icloud.com>
2026-09-06 15:28:39 +0700

    loggerEditedagain

--- utils/logger.py
+++ utils/logger.py
@@ -3,11 +3,9 @@ import logging
 
 def get_logger(name=__name__):
   logger = logging.getLogger(name)
+  # Если хэндлеров еще нет, просто задаем уровень.
+  # Никаких StreamHandler создавать НЕ нужно!
   if not logger.handlers:
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
     logger.setLevel(logging.INFO)
   return logger
 
